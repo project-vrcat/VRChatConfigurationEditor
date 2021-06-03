ad241960bf567f9f46a0bcee23db896ea2482706
CunYu <cunyu@liuli.lol>
2021-06-03 13:49:30 +0800

    style: Improve structure

--- main.go
+++ main.go
@@ -2,11 +2,7 @@
 package main
 
 import (
-	"embed"
 	"fmt"
-	"io/fs"
-	"net"
-	"net/http"
 	"net/url"
 
 	"github.com/TheTitanrain/w32"
@@ -40,32 +36,4 @@ func main() {
 	_ = ui.Load(fmt.Sprintf("http://127.0.0.1:%d", port))
 
 	<-ui.Done()
-}
-
-func server() int {
-	port := pickPort()
-	http.Handle("/", http.StripPrefix("/", http.FileServer(getFileSystem())))
-	go func() {
-		_ = http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", port), nil)
-	}()
-	return port
-}
-
-func pickPort() int {
-	listener, err := net.Listen("tcp4", "127.0.0.1:0")
-	if err != nil {
-		return -1
-	}
-	defer listener.Close()
-
-	addr := listener.Addr().(*net.TCPAddr)
-	return addr.Port
-}
-
-//go:embed public
-var publicFiles embed.FS
-
-func getFileSystem() http.FileSystem {
-	fSys, _ := fs.Sub(publicFiles, "public")
-	return http.FS(fSys)
 }
